2351bfe2db25d3217ec3ed872f50d68d34441e62
james <james@james.tapping@gmail.com>
2022-08-05 09:03:10 +0200

    Initial Commit

---
+++ SpaceX-API-Demo/SpaceX_API_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_API_DemoApp.swift
+//  SpaceX-API-Demo
+//
+//  Created by james on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_API_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
